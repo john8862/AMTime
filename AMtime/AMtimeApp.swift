293106193e37177c6bfb4c8e3532a43cd76a59eb
john8862 <sherlock.zhao@outlook.com>
2024-04-27 22:11:45 +1000

    Initial Commit

---
+++ AMtime/AMtimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AMtimeApp.swift
+//  AMtime
+//
+//  Created by Sherlock Zhao on 27/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct AMtimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AMtime/Extensions/ColorExt.swift
@@ -0,0 +1,21 @@
+//
+//  ColorExt.swift
+//  AMtime
+//
+//  Created by Sherlock Zhao on 27/4/2024.
+//
+
+import SwiftUI
+
+extension Color {
+    static let darkPurple = Color("darkPurple")
+    static let darkYellow = Color("darkYellow")
+    static let lightGray = Color("lightGray")
+    static let lightPurple = Color("lightPurple")
+    static let lightYellow = Color("lightYellow")
+    static let purpleGray = Color("purpleGray")
+    static let accent = Color("red")
+    static let clearPurple = Color("clearPurple")
+    static let textColor = Color("textColor")
+    static let background = Color("background")
+}
