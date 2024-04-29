6ac28333c23f876b826e0efa559bdfc5c688aa70
scel665 <songzerui82@gmail.com>
2024-04-30 00:11:17 +1000

    feat: update the content view structure

---
+++ AMtime/Views/DynamicContainer.swift
@@ -0,0 +1,18 @@
+//
+//  DynamicContainer.swift
+//  AMtime
+//
+//  Created by Mac on 29/4/2024.
+//
+
+import SwiftUI
+
+struct DynamicContainer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DynamicContainer()
+}
---
+++ AMtime/Views/SectionView.swift
@@ -0,0 +1,18 @@
+//
+//  SectionView.swift
+//  AMtime
+//
+//  Created by Mac on 29/4/2024.
+//
+
+import SwiftUI
+
+struct SectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SectionView()
+}
